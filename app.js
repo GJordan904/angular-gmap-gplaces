6c5cfb2e2f46418737a713893b8ce5b0e480c309
Grant <grant.jordan904@gmail.com>
2016-09-11 14:20:31 -0400

    copied from original

---
+++ app.js
@@ -0,0 +1,82 @@
+(function() {'use strict';
+
+angular.module('myApp', [
+	'app.controllers',
+	'angular-gmap-gplace',
+	'hljs',
+	'ui.router',
+	'ngAnimate'
+])
+.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('app', {
+			url: '',
+			abstract: true,
+			templateUrl: 'views/main.html'
+		})
+		.state('app.home', {
+			url: '/home',
+			views: {
+				'mainContent': {
+					templateUrl: 'views/home.html'
+				}
+			}
+		})
+		.state('app.docsPlaces', {
+			url: '/docs/places',
+			views: {
+				'mainContent': {
+					templateUrl: 'views/docsPlaces.html'
+				}
+			}
+		})
+		.state('app.docsMap', {
+			url: '/docs/map',
+			views: {
+				'mainContent': {
+					templateUrl: 'views/docsMap.html'
+				}
+			}
+		})
+		.state('app.exPlace', {
+			url: '/ex/place',
+			views: {
+				'mainContent': {
+					templateUrl: 'views/exPlace.html',
+					controller: 'PlacesCtrl as places'
+				}
+			}
+		})
+		.state('app.exPlaces', {
+			url: '/ex/places',
+			views: {
+				'mainContent': {
+					templateUrl: 'views/exPlaces.html',
+					controller: 'PlacesCtrl as places'
+				}
+			}
+		})
+		.state('app.exMap', {
+			url: '/ex/map',
+			views: {
+				'mainContent': {
+					templateUrl: 'views/exMap.html',
+					controller: 'MapCtrl as map'
+				}
+			}
+		});
+
+	$urlRouterProvider.otherwise('/home');
+})
+.directive('showTab', function() {
+	return {
+		link: function (scope, element, attrs) {
+			element.click(function(e) {
+				e.preventDefault();
+				$(element).tab('show');
+			});
+		}
+	};
+});
+
+}());
---
+++ js/controllers.js
@@ -0,0 +1,74 @@
+(function() {'use strict';
+
+    angular.module('app.controllers', [])
+        .controller('AppCtrl', function () {
+            var vm = this;
+        })
+
+        .controller('PlacesCtrl', function () {
+            var vm = this;
+
+            // Single request
+            vm.id = 'ChIJR4dOl_hYwokRApSCaQiBidk';
+
+            // Request for multiple locations model
+            vm.idModel = [
+                'ChIJR4dOl_hYwokRApSCaQiBidk',
+                'ChIJv-Ghof5YwokRMtWLEV12hJI',
+                'ChIJjyX2GqRZwokRT-gdcGoPuSI',
+                'ChIJqSurReFYwokRec7JFACToas',
+                'ChIJn8dCo-NYwokRC_4nRUQWbNE',
+                'ChIJszmN0-JYwokRk-XCDbO6X_Y',
+                'ChIJt4TrE_1YwokRVedrKxaqYoo',
+                'ChIJiW0WvwJZwokRIWyzCvo3o5k',
+                'ChIJsS1xLQJZwokRGfXJPMwXA1A',
+                'ChIJI5xCX6NZwokR3jdSQwsw2DI',
+                'ChIJuVE5aLtZwokR-K75OxUEtzI',
+                'ChIJ7R4tgLtZwokRM8thlhlzE2o',
+                'ChIJxbWTG_pYwokRPgtFVKi-Cuc',
+                'ChIJKZVnwFVYwokRgDw_sxw3NCo',
+                'ChIJw_JUgvhYwokR91EMxVDhB8M',
+                'ChIJZ3oXOVZYwokRNnAXaDRKAzg',
+                'ChIJP9idxlZYwokRLH-I1mNfzYQ',
+                'ChIJndGJ5FNYwokRricJvhT0t1s',
+                'ChIJhZ0Sn1ZYwokRRA1MZJZHrHA',
+                'ChIJhTdv51NYwokR7V105uVzf8g',
+                'ChIJvfVwDFJYwokRtWobbwOMEVM',
+                'ChIJpY9Tg01YwokRCr_aQpDrqgk',
+                'ChIJ-fRuLFdYwokR0KKQ6Av_WhQ',
+                'ChIJh-DIsE1YwokRhuFrdM1ge5E'
+            ];
+        })
+
+        .controller('MapCtrl', function () {
+            var vm = this;
+
+            vm.mapOptions = {
+                center: {lat: 29.892410, lng: -81.31445},
+                zoom: 10,
+                mapTypeId: 'roadmap'
+            };
+
+            vm.marker = {
+                position: {lat: 29.892410, lng: -81.31445},
+                animation: google.maps.Animation.DROP
+            };
+
+            vm.click = function (arg) {
+                if (arg.getAnimation() !== null) {
+                    arg.setAnimation(null);
+                    console.log('clicked true');
+                } else {
+                    arg.setAnimation(google.maps.Animation.BOUNCE);
+                    console.log('clicked false');
+                }
+            };
+
+            vm.directions = {
+                origin: '625 8th Avenue, New York, NY, 10018',
+                destination: '260 Broadway New York NY 10007',
+                travelMode: 'WALKING'
+            };
+        });
+
+}());
